b08c8237503e5d1827e886b5f526f60c79fbb371
joyboy <josc0012@uw.edu>
2024-07-20 17:50:57 +0200

    David changes

--- Side-Channel-CtrlF/run.py
+++ Side-Channel-CtrlF/run.py
@@ -6,6 +6,7 @@ app, app_measure = create_app()
 
 if __name__ == '__main__':
     server1 = WSGIServer(("0.0.0.0", 5000), app)
+    print("Running on http://localhost:5000/")
     server1.start()
 
     server2 = WSGIServer(("0.0.0.0", 5001), app_measure)
